946f9198fb3ea50a9424f1fdf1d6a63ac8adba9f
LoverGreenTea <mgrican11@gmail.com>
2025-04-06 13:37:41 +0300

    3

--- Main.py
+++ Main.py
@@ -3,12 +3,16 @@ import json
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+player_data = {}
 class GamePlayWindow(Screen):
     def __init__(self, **kw):
             super().__init__(**kw)
 
-
-
+def click(self):
+    score = int(self.ids.score_lbl.text)
+    score += 1
+    self.ids.score_lbl.text = str(score)
+    self.ids.ClickerIcon.img.size_hint = (0.5, 0.5)
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
